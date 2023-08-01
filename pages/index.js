5d54c969e68b74511ce155d1aadc4f730e068349
Adriano Ferreira <adriano.ferreira@ab-inbev.com>
2023-08-01 11:43:28 -0300

    init project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <p>
+        O que eu mais amo na minha casa é quem divide ela comigo, nas alegrias e
+        nas tristezas.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
